a745ee9e63c0fdf98fdcc80bd5ea3462f65e89eb
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-16 13:14:42 +0300

    Commit

--- AspNetCoreIdentity.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentity.Web/Controllers/MemberController.cs
@@ -1,9 +1,11 @@
 ﻿using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreIdentity.Web.Controllers
 {
+    [Authorize]
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
@@ -11,6 +13,11 @@ namespace AspNetCoreIdentity.Web.Controllers
         public MemberController(SignInManager<AppUser> signInManager)
         {
             _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         public async Task LogOut()
