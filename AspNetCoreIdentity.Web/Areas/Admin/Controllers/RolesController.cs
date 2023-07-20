97e27c09fb01e00f6a4329019baf605f7b937f9d
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-20 23:28:43 +0300

    Commit

--- AspNetCoreIdentity.Web/Areas/Admin/Controllers/RolesController.cs
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RolesController.cs
@@ -4,6 +4,7 @@ using AspNetCoreIdentity.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
 {
@@ -47,7 +48,85 @@ namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
                 return View();
             }
 
+            TempData["SuccessMessage"] = "Rol eklenmiştir.";
             return RedirectToAction(nameof(RolesController.Index));
         }
+
+        public async Task<IActionResult> RoleUpdate(string id)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(id);
+
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamamıştır");
+            }
+
+            return View(new RoleUpdateViewModel() {Id=roleToUpdate.Id,Name=roleToUpdate!.Name });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleUpdate(RoleUpdateViewModel request)
+        {
+            var roleToUpdate=await _roleManager.FindByIdAsync(request.Id);
+
+            if (roleToUpdate == null)
+                throw new Exception("Güncellenecek rol bulunamamıştır.");
+
+            roleToUpdate.Name=request.Name;
+            await _roleManager.UpdateAsync(roleToUpdate);
+
+            // Aynı sayfaya yönlendireceksek viewData kullanırız.
+            ViewData["SuccessMessage"] = "Rol bilgisi güncellenmiştir.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> RoleDelete(string id)
+        {
+            var roleToDelete = await _roleManager.FindByIdAsync(id);
+
+            if (roleToDelete == null)
+                throw new Exception("Silinecek rol bulunamamıştır");
+
+            var result=await _roleManager.DeleteAsync(roleToDelete);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.Select(x => x.Description).FirstOrDefault());
+            }
+
+            TempData["SuccessMessage"] = "Rol silinmiştir.";
+            return RedirectToAction(nameof(RolesController.Index));
+        }
+
+        public async Task<IActionResult> AssignRoleToUser(string id)
+        {
+            var currentUser = await _userManager.FindByIdAsync(id);
+
+            if (currentUser == null)
+                throw new Exception("Kullanıcı bulunamamıştır");
+
+            var roles=await _roleManager.Roles.ToListAsync();
+
+            var userRoles=await _userManager.GetRolesAsync(currentUser);
+
+            var roleViewModelList = new List<AssignRoleToUserViewModel>();
+
+            foreach (var role in roles)
+            {
+                var assignRoleToUserViewModel=new AssignRoleToUserViewModel() { Id = role.Id ,Name=role.Name!};
+
+                if (userRoles.Contains(role.Name!))
+                    assignRoleToUserViewModel.Exist = true;
+
+                roleViewModelList.Add(assignRoleToUserViewModel);
+
+                
+                    
+            }
+
+            return View(roleViewModelList);
+        }
+
     }
 }
