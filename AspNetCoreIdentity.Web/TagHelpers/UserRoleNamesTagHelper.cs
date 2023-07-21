c446681428214837038e6783398f0ace2b581432
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-21 12:23:17 +0300

    Commit

---
+++ AspNetCoreIdentity.Web/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace AspNetCoreIdentity.Web.TagHelpers
+{
+    public class UserRoleNamesTagHelper:TagHelper
+    {
+        public string UserId { get; set; } = null!;
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleNamesTagHelper(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var user = await _userManager.FindByIdAsync(UserId);
+
+            var userRoles=await _userManager.GetRolesAsync(user!);
+
+
+            // StringBuilder string'leri yan yana ekler
+            var stringBuilder = new StringBuilder();
+
+            userRoles.ToList().ForEach(x =>
+            {
+                stringBuilder.Append($@"<span class='badge-pill badge-primary mx-1'>{x.ToLower()}</span>");
+            });
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+
+
+           
+        }
+    }
+}
